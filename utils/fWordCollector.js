2e26aebb16ccd906421138c65e4828be370ceaf0
HJY <hwangjae1139@gmail.com>
2025-02-10 16:37:13 +0900

    [fix]: 욕 필터링 로직 수정

--- utils/fWordCollector.js
+++ utils/fWordCollector.js
@@ -3,6 +3,13 @@ const { badWords } = require("../config/badwords.json");
 const fWordCollector = (message) => {
     const msgSplit = message.split(" ");
     const badWordsInMsg = msgSplit.filter((word) => badWords.includes(word));
+    msgSplit.forEach((word) => {
+        badWords.forEach((badWord) => {
+            if (word.includes(badWord)) {
+                return true;
+            }
+        });
+    });
     if (badWordsInMsg.length > 0) {
         return true;
     }
