17805604a70499de9d8943bd7750f004cc8b1717
HJY <hwangjae1139@gmail.com>
2025-02-10 16:42:18 +0900

    [fix]: 욕 필터링 변경

--- utils/fWordCollector.js
+++ utils/fWordCollector.js
@@ -2,18 +2,10 @@ const { badWords } = require("../config/badwords.json");
 
 const fWordCollector = (message) => {
     const msgSplit = message.split(" ");
-    const badWordsInMsg = msgSplit.filter((word) => badWords.includes(word));
-    msgSplit.forEach((word) => {
-        badWords.forEach((badWord) => {
-            if (word.includes(badWord)) {
-                return true;
-            }
-        });
-    });
-    if (badWordsInMsg.length > 0) {
-        return true;
-    }
-    return false;
+    const hasBadWord = msgSplit.some((word) => 
+        badWords.some((badWord) => word.includes(badWord))
+    );
+    return hasBadWord;
 }
 
 module.exports = { fWordCollector };
