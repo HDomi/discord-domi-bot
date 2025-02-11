5dfa6d27106799a485ccca04bd052f39df3d29dc
HJY <hwangjae1139@gmail.com>
2025-02-11 15:52:41 +0900

    [fix]: 네임변경

--- commands/cmdSteam.js
+++ commands/cmdSteam.js
@@ -7,12 +7,12 @@ module.exports = {
         .setName('스팀')
         .setDescription('스팀 게임 정보')
         .addStringOption(option => 
-            option.setName('스팀아이디')
+            option.setName('스팀 커뮤니티 ID')
             .setDescription('스팀 커뮤니티 ID를 입력하세요(내 프로필 누르면 상단 주소)')
             .setRequired(true)),
     async execute(interaction) {
         try {
-            const userInput = interaction.options.getString('스팀아이디') || '기본값';
+            const userInput = interaction.options.getString('스팀 커뮤니티 ID') || '기본값';
             const userId = await getSteamUserId(userInput);
             if (!userId) {
                 await interaction.reply({ content: '존재하지 않거나 비공개 처리된 스팀 아이디입니다.', ephemeral: true });
@@ -33,7 +33,7 @@ module.exports = {
                 .setColor(0x426cf5)
                 .setTitle(`당신의 스팀정보`)
                 .addFields(
-                    { name: `스팀아이디`, value: userInput },
+                    { name: `스팀 커뮤니티 ID`, value: userInput },
                     { name: '보유 게임 수', value: String(hasGame.length) },
                     { name: '---------------------------------------', value: '<Play Top3 Game>' },
                     ...getTop3Game(),
