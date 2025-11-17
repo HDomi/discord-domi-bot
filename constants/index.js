e3dad464782a00c95e7f9ff4a9d7487d13e96a86
HJY <hwangjae1139@gmail.com>
2025-11-17 09:12:13 +0900

    fix: 구문에러 수정

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,8 @@
 const MAIN = 'DOMI_BOT'
-export const FIREBASE_CONSTANTS = {
-    ATTENDANCE: `${MAIN}/출석`,
+const FIREBASE_CONSTANTS = {
+  ATTENDANCE: `${MAIN}/출석`,
+}
+
+module.exports = {
+  FIREBASE_CONSTANTS,
 }
